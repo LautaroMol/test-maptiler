ed098f6e2ce415dc84b24cd10e1d97e2d0f924d5
Lautaro Castellanos <laumol159@gmail.com>
2024-09-02 09:28:51 -0300

    platform

--- src/app/pages/mapa/mapa.component.ts
+++ src/app/pages/mapa/mapa.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, OnDestroy, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { Map, MapStyle, config } from '@maptiler/sdk';
 import '@maptiler/sdk/dist/maptiler-sdk.css';
 
@@ -7,29 +8,37 @@ import '@maptiler/sdk/dist/maptiler-sdk.css';
   standalone: true,
   imports: [],
   templateUrl: './mapa.component.html',
-  styleUrl: './mapa.component.css',
+  styleUrls: ['./mapa.component.css'],
 })
 export class MapaComponent implements OnInit, AfterViewInit, OnDestroy {
   map: Map | undefined;
   geo: any;
   @ViewChild('map')
   private mapContainer!: ElementRef<HTMLElement>;
-  constructor() {}
+
+  constructor(@Inject(PLATFORM_ID) private platformId: any) {}
+
   ngOnDestroy(): void {
-    this.map?.remove();
+    if (isPlatformBrowser(this.platformId)) {
+      this.map?.remove();
+    }
   }
 
   ngOnInit(): void {
-    config.apiKey = 'oEDh6mPK2TIhdFrpa70J';
+    if (isPlatformBrowser(this.platformId)) {
+      config.apiKey = 'oEDh6mPK2TIhdFrpa70J';
+    }
   }
 
   ngAfterViewInit(): void {
-    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
-  this.map = new Map({
-    container: this.mapContainer.nativeElement,
-    style: MapStyle.STREETS,
-    center: [initialState.lng, initialState.lat],
-    zoom: initialState.zoom
-  });
+    if (isPlatformBrowser(this.platformId)) {
+      const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+      this.map = new Map({
+        container: this.mapContainer.nativeElement,
+        style: MapStyle.STREETS,
+        center: [initialState.lng, initialState.lat],
+        zoom: initialState.zoom,
+      });
+    }
   }
 }
